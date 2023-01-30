28b50b92f33e6724cb18e09519a22cd4c9a3af5b
tatazoe <takashi.tazoe@gmail.com>
2023-01-31 06:23:12 +0900

    :add: Add category add UI

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+window.axios = require('axios')
+import swal from 'sweetalert';
+window.Swal = swal
+
 createApp(App).use(router).mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/HomeView.vue'
+import AddCategory from "../views/Category/AddCategory";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/admin/category/add",
+    name: "AddCategory",
+    component: AddCategory,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
